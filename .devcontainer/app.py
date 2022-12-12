40d29f529805dfc0adc363f8a6702088637de3b6
Jay Miller <kjaymiller@gmail.com>
2022-12-12 19:32:54 +0000

    first commit

---
+++ .devcontainer/app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+@app.route('/hello/<name>')
+def hello_name(name):
+    return f'Hello {name}!'
+
+
+if __name__ == '__main__':
+    app.run()
